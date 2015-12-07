c816fc3d9e2723016e498e9dbc32f896b9d8396c
praskura <gregorypraskura@hotmail.com>
2015-12-07 23:03:39 +0300

    Commit #2

--- app/src/main/java/ru/startandroid/testapp/MenuFragment.java
+++ app/src/main/java/ru/startandroid/testapp/MenuFragment.java
@@ -3,7 +3,6 @@ package ru.startandroid.testapp;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
