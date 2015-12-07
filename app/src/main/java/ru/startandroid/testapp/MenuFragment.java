27b28632da5749b70ce4e88429fb86c58fdc78b6
praskura <gregorypraskura@hotmail.com>
2015-12-07 22:58:25 +0300

    Initial commit

---
+++ app/src/main/java/ru/startandroid/testapp/MenuFragment.java
@@ -0,0 +1,19 @@
+package ru.startandroid.testapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dns1 on 07.12.2015.
+ */
+public class MenuFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.users_layout, container, false);
+    }
+}
